e799c31bcc8ad7edd16af468fbd9751258196cb7
Marc Samba <marc.samba@etu.minesparis.psl.eu>
2024-02-02 11:27:56 +0100

    class ennemy

--- rogue.py
+++ rogue.py
@@ -12,14 +12,22 @@ class Character:
         key = self.stdscr.getch()
         if key == ord('q'):
             self.run = False
-        elif key == curses.KEY_UP and self.y > 1:
+        elif key == ord('e') and self.y > 1:
             self.y -= 1
-        elif key == curses.KEY_DOWN:
+        elif key == ord('x'):
             self.y += 1
-        elif key == curses.KEY_RIGHT:
+        elif key == ord('d'):
             self.x += 1
-        elif key == curses.KEY_LEFT and self.x > 1:
+        elif key == ord('s') and self.x > 1:
             self.x -= 1
+
+class ennemy:
+    def __init__(self,stdscr):
+        self.ennemy_x=None
+        self.ennemy_y=None
+
+        
+
 
 class Dungeon:
     def __init__(self, stdscr):
@@ -38,6 +46,9 @@ class Dungeon:
             for x, tile in enumerate(row):
                 self.player.stdscr.addch(y, x, tile)
         self.player.stdscr.addch(self.player.y, self.player.x, '@')
+
+    def gain(self):
+        pass
 
 def main(stdscr):
     dungeon = Dungeon(stdscr)
